235caeb682edfced54f0e740b304cae602120add
Paweł Wilk <siefca@gnu.org>
2023-01-31 22:55:05 +0100

    Fixed a bug causing first character of code to never match

--- src/amelinium/model/confirmation.clj
+++ src/amelinium/model/confirmation.clj
@@ -38,7 +38,7 @@
 (defn gen-code
   []
   (let [code (format "%07d" (unchecked-int (rand 9999999)))]
-    (if (= (first code) "0")
+    (if (= (first code) \0)
       (str (inc (rand-int 8)) (subs code 1))
       code)))
 
