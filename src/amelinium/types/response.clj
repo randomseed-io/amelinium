00dd1520f83b167bb6af78b85402144b2bd8599d
Paweł Wilk <siefca@gnu.org>
2023-06-18 19:21:23 +0200

    Added types/response

---
+++ src/amelinium/types/response.clj
@@ -0,0 +1,15 @@
+(ns
+
+    ^{:doc    "amelinium service, response record type."
+      :author "Paweł Wilk"
+      :added  "1.0.0"}
+
+    amelinium.types.response
+
+  (:require [amelinium]))
+
+(in-ns 'amelinium)
+
+(defrecord Response [^long                        status
+                     ^clojure.lang.IPersistentMap headers
+                     ^String                      body])
