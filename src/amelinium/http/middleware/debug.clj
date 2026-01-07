a710a3ed721f116bef4213f4c3749cd721dd1aec
Paweł Wilk <siefca@gnu.org>
2026-01-08 00:56:24 +0100

    Added zprint, improved debugging middleware

--- src/amelinium/http/middleware/debug.clj
+++ src/amelinium/http/middleware/debug.clj
@@ -11,32 +11,37 @@
   (:require [clojure.string          :as         str]
             [amelinium.logging       :as         log]
             [amelinium.system        :as      system]
-            [puget.printer           :refer [cprint]]))
+            [zprint.core             :refer [zprint czprint]]))
+
+(defonce ^:private counter  (atom 0))
+(defonce ^:private icounter (atom 0))
 
 ;; Configuration initializers
 
 (defn wrap
   "Debugging middleware."
-  [k {:keys [enabled? request-keys response-keys]
+  [k {:keys [enabled? request-keys response-keys tag]
       :or   {enabled? true}}]
   (when enabled?
-    (log/msg "Initializing debugging middleware")
-    (let [request-keys  (if (seq request-keys)  request-keys)
-          response-keys (if (seq response-keys) response-keys)]
-      {:name    k
-       :compile (fn [_ _]
-                  (fn [handler]
-                    (fn [req]
-                      (when (some? request-keys)
-                        (println "--------")
-                        (println "REQUEST:")
-                        (cprint (select-keys req request-keys)))
-                      (let [resp (handler req)]
-                        (when (some? response-keys)
-                          (println "---------")
-                          (println "RESPONSE:")
-                          (cprint (select-keys resp response-keys)))
-                        resp))))})))
+    (let [id  (swap! counter inc)
+          tag (or tag (str (name k) "#" id))]
+      (log/msg "Initializing debugging middleware")
+      (let [request-keys  (if (seq request-keys)  request-keys)
+            response-keys (if (seq response-keys) response-keys)]
+        {:name    k
+         :compile (fn [_ _]
+                    (let [iid  (swap! icounter inc)
+                          itag (str iid)]
+                      (fn [handler]
+                        (fn [req]
+                          (println "--------" itag "@" tag "IN  req-nil?" (nil? req) "type" (type req))
+                          (when request-keys
+                            (czprint (select-keys req request-keys)))
+                          (let [resp (handler req)]
+                            (println "--------" itag "@" tag "OUT resp-nil?" (nil? resp) "type" (type resp))
+                            (when (some? response-keys)
+                              (czprint (select-keys resp response-keys)))
+                            resp)))))}))))
 
 (system/add-init  ::default [k config] (wrap k config))
 (system/add-halt! ::default [_ config] nil)
